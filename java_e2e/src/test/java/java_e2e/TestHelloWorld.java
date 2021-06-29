658069e3a1cf1c667de155b75311ff50c8c46ef6
Arshad <arshad.mk1612@gmail.com>
2021-06-29 19:09:21 +0530

    creating new project

---
+++ java_e2e/src/test/java/java_e2e/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package java_e2e;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>Hello world<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		}
+}
